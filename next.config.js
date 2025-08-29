79103ad51e995bcef4b83897c42945c6e5f9c9a4
felipeacx <juanfe1190@gmail.com>
2025-08-29 14:55:52 -0500

    Configure Next.js to allow remote images from Rick and Morty API

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// next.config.js
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rickandmortyapi.com",
+        pathname: "/api/character/avatar/**",
+      },
+    ],
+  },
+}
