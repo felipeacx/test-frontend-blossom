e3f556873f9a3d6cf3daa1e72a4cc648c57295f9
felipeacx <juanfe1190@gmail.com>
2025-08-29 12:55:07 -0500

    Refactor NavigationBar to include filtering logic

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -7,11 +7,33 @@ import React, { useMemo } from "react"
 import { FiSearch } from "react-icons/fi"
 import { PiSliders } from "react-icons/pi"
 import { FiHeart } from "react-icons/fi"
+import FilterCharacters from "./FilterCharacters"
+import { AiFillHeart } from "react-icons/ai"
 
 const NavigationBar: React.FC = () => {
-  const { characters, setCharacters } = useCharacters()
+  const { characters, setShowFilter, showFilter, filteredCharacter, filteredSpecie } =
+    useCharacters()
 
-  const memoizedCharacters = useMemo(() => characters, [characters])
+  // Memoized filter calculations
+  const memoizedCharacters = useMemo(() => {
+    return characters.filter((char) => {
+      const matchesName =
+        filteredCharacter && filteredCharacter === "all"
+          ? !char.starred
+          : filteredCharacter === "others"
+          ? true
+          : filteredCharacter === "starred" && char.starred
+      const matchesSpecies =
+        filteredSpecie && filteredSpecie !== "all"
+          ? char.species.toLowerCase().includes(filteredSpecie.toLowerCase())
+          : true
+      return matchesName && matchesSpecies
+    })
+  }, [characters, filteredCharacter, filteredSpecie])
+
+  const onClickFilter = () => {
+    setShowFilter(!showFilter)
+  }
 
   return (
     <nav className="w-1/3 backdrop-blur-lg shadow-lg flex flex-col">
@@ -25,9 +47,13 @@ const NavigationBar: React.FC = () => {
           className="w-full pl-12 pr-10 py-4 rounded-lg border bg-[#F3F4F6] h-[52px] text-[#6B7280] text-sm leading-5"
           placeholder="Search or filter results"
         />
-        <PiSliders className="absolute right-4 top-1/2 -translate-y-1/2 text-2xl text-primary-600 cursor-pointer" />
+        <PiSliders
+          onClick={onClickFilter}
+          className="absolute right-4 top-1/2 -translate-y-1/2 text-2xl text-primary-600 cursor-pointer hover:scale-110 transition-transform duration-200 ease-in-out hover:bg-primary-100 hover:rounded-lg"
+        />
+        <FilterCharacters />
       </div>
-      <ul className="p-4 grid grid-cols-6 gap-3 overflow-auto h-[calc(100svh-180px)]">
+      <ul className="p-4 grid grid-cols-6 gap-3 overflow-auto max-h-[calc(100svh-180px)]">
         <li className="col-span-6 h-[56px] flex justify-start items-center pl-5">
           <p className="text-xs leading-4 uppercase text-gray-500 tracking-[5%]">
             Characters ({memoizedCharacters.length})
@@ -51,7 +77,11 @@ const NavigationBar: React.FC = () => {
               </div>
             </div>
             <div className="col-span-1 flex justify-center items-center">
-              <FiHeart className="text-2xl" />
+              {char.starred ? (
+                <AiFillHeart className="text-4xl text-[#53C629] bg-white rounded-3xl p-1" />
+              ) : (
+                <FiHeart className="text-2xl" />
+              )}
             </div>
           </li>
         ))}
