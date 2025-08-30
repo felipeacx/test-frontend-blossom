136c3bde8e4cbf2ab2d1e902007d2c0f739e0071
felipeacx <juanfe1190@gmail.com>
2025-08-30 14:42:12 -0500

    Jest configuration and packages installed, initial tests added.

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const nextJest = require("next/jest")
+
+const createJestConfig = nextJest({
+  dir: "./",
+})
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }],
+  },
+  transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"],
+}
+
+module.exports = createJestConfig(customJestConfig)
